14e07e4b257ebb57f488d3a67e839051789cecd4
Cengizhan Tomak <cengizhantomak@gmail.com>
2022-08-25 16:29:23 +0300

    Kullanici Arayuzu

---
+++ TravelBook/ListViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ListViewController.swift
+//  TravelBook
+//
+//  Created by Tuna Tomak on 25.08.2022.
+//
+
+import UIKit
+
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    @objc func addButtonClicked() {
+        performSegue(withIdentifier: "toViewController", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "test"
+        return cell
+    }
+    
+
+}
